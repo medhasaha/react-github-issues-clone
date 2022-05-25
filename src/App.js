ad057736d287613ed75deedea427ba6cbc0410b7
Medha Saha <medha.saha@hyperdart.com>
2022-05-26 01:23:31 +0530

    MS - icons + responsive

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import { withStyles } from '@material-ui/core/styles';
 import {Grid, Typography, Chip, Divider, Tabs, Tab} from '@material-ui/core'
 import InfiniteScroll from "react-infinite-scroll-component";
 import IssuesItem from "./IssuesItem";
+import withWidth from '@material-ui/core/withWidth';
+
+import CodeIcon from '@material-ui/icons/Code';
+import CircleIcon from '@material-ui/icons/Adjust';
+import ForkIcon from '@material-ui/icons/CallSplit';
+import PullRequestIcon from '@material-ui/icons/AccountTree';
+import ActionIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import ProjectIcon from '@material-ui/icons/Assessment';
+import WikiIcon from '@material-ui/icons/MenuBook';
+import SecurityIcon from '@material-ui/icons/VerifiedUser';
+import InsightIcon from '@material-ui/icons/TrendingUp';
+import CheckIcon from '@material-ui/icons/Check';
 
 const style = theme => ({
   div : {
@@ -18,25 +30,75 @@ const style = theme => ({
     "border-bottom" : "1px grey solid",
     padding : "30px 20px 0px 20px"
   },
-  chip : {
-    margin : "0px 0px 0px 10px"
+  secondHeader : {
+    width : "100%",
+    "background-color" : "#e9f5f8",
+    padding : "20px 20px 20px 20px"
   },
-  chipDiv : {
+  chip : {
+    margin : "0px 0px 0px 10px",
+    fontWeight : 600,
+    verticalAlign : "initial"
+  },
+  customChip : {
     border : "1px rgba(0, 0, 0, 0.23) solid",
     borderRadius : "5px",
     display: "inline-block",
-    padding : "5px",
+    // padding : "5px",
     margin : "0px 10px",
-    float : "right"
+    [theme.breakpoints.down('md')]: {
+      marginTop : "5px",
+      marginBottom : "5px"
+    },
+    [theme.breakpoints.up('md')]: {
+      float : "right"
+    },
+  },
+  typoCustomChip : {
+    fontWeight : 600,
+    fontSize : "13px"
   },
   verticalDiv : {
-    margin : "0px 10px"
+    margin : "0px 0px"
   },
   tabItem : {
     fontWeight : 800
   },
   indicator: {
     backgroundColor: 'orange',
+  },
+  icon : {
+    fotSize : "20px",
+    margin: "0px 10px 0px 0px",
+    verticalAlign : "bottom",
+    color : "rgba(0, 0, 0, 0.23)"
+  },
+  checkIcon : {
+    fotSize : "20px",
+    margin: "0px 10px 0px 20px",
+    verticalAlign : "bottom",
+    color : "rgba(0, 0, 0, 0.23)"
+  },
+  tablabel : {
+    fontWeight : 500,
+    color : "#000",
+    fontSize : "14px"
+  },
+  issuesGrid : {
+    [theme.breakpoints.up('md')]: {
+      margin : "20px 100px 0px 100px",
+    },
+    overflowX : "hidden",
+    border : "1px rgba(0, 0, 0, 0.23) solid",
+    "border-top-style": "solid",
+    "border-right-style": "solid",
+    "border-left-style": "solid",
+  },
+  infiniteScroll : {
+    width : "100vw",
+    [theme.breakpoints.down('md')]: {
+      width : "100vw"
+    },
   }
 })
 
@@ -67,7 +129,7 @@ class App extends React.Component {
       this.setState(prevState => ({
         data: prevState.data.concat(res),
         page: prevState.page + 1,
-        hasMore : prevState.page === 5 ? false : true
+        hasMore : prevState.page === 10 ? false : true
       }))
 
     });
@@ -79,21 +141,35 @@ class App extends React.Component {
     return (
       <Grid container> 
         <Grid container className = {classes.header}>
-          <Grid item xs = {6} >
+          <Grid item md = {6} xs = {12}>
             <Typography variant = "h6" style = {{display : "inline", color : "#0969da"}}>facebook</Typography>
             <Typography variant = "h6" style = {{display : "inline"}}> / </Typography>
             <Typography variant = "h6" style = {{display : "inline", color : "#0969da", fontWeight : 600}}>react</Typography>
             <Chip label="Public" variant="outlined" size = "small" className={classes.chip}/>
           </Grid>
 
-          <Grid item xs = {6} >
-            <div className={classes.chipDiv} style = {{display : "inline-flex"}}> 
-              <Typography style = {{display : "flex"}}>Fork <Divider orientation="vertical" flexItem  className = {classes.verticalDiv}/> 35.3k</Typography>
+          <Grid item md = {6} xs = {12}>
+            <Grid className={classes.customChip} style = {{display : "inline-flex"}}> 
+              <Grid item style = {{padding : "5px 5px 5px 10px"}}>
+                <Typography style = {{display : "flex"}} className = {classes.typoCustomChip}>Fork </Typography>
+              </Grid>
+              <Divider orientation="vertical" flexItem  className = {classes.verticalDiv}/> 
+              <Grid item style = {{padding : "5px 10px 5px 5px", backgroundColor : "#fff"}}>
+                <Typography className = {classes.typoCustomChip}>35.3k</Typography>
+              </Grid>
+            </Grid>
+            <Grid className={classes.customChip} style = {{display : "inline-flex"}}> 
+              <Grid item style = {{padding : "5px 5px 5px 10px"}}>
+                <Typography style = {{display : "flex"}} className = {classes.typoCustomChip}>Star </Typography>
+              </Grid>
+              <Divider orientation="vertical" flexItem  className = {classes.verticalDiv}/> 
+              <Grid item style = {{padding : "5px 10px 5px 5px", backgroundColor : "#fff"}}>
+                <Typography className = {classes.typoCustomChip}>175k</Typography>
+              </Grid>
+            </Grid>
+            <div className={classes.customChip} style = {{padding : "5px"}}>
+              <Typography className = {classes.typoCustomChip}>Notifications</Typography>
             </div>
-            <div className={classes.chipDiv} style = {{display : "inline-flex"}}> 
-              <Typography style = {{display : "flex"}}>Star <Divider orientation="vertical" flexItem  className = {classes.verticalDiv}/> 175k</Typography>
-            </div>
-            <div className={classes.chipDiv}><Typography>Notifications</Typography></div>
           </Grid>
 
           <Tabs
@@ -102,32 +178,39 @@ class App extends React.Component {
           textColor="primary"
           variant="scrollable"
           scrollButtons="auto"
-          style = {{marginTop : "20px"}} classes={{
+          style = {{marginTop : "20px"}} 
+          classes={{
             indicator: classes.indicator
           }}>
-            <Tab label="Code" className={classes.tabItem}/>
-            <Tab label="Issues"  />
-            <Tab label="Pull Requests"  />
-            <Tab label="Actions" />
-            <Tab label="Projects"  />
-            <Tab label="Wiki"  />
-            <Tab label="Security"  />
-            <Tab label="Insights"  />
+            <Tab label={<Typography className = {classes.tablabel}><CodeIcon className={classes.icon}/>Code</Typography>}/>
+            <Tab label={<Typography className = {classes.tablabel}><CircleIcon className={classes.icon}/>Issues</Typography>} />
+            <Tab label={<Typography className = {classes.tablabel}><PullRequestIcon className={classes.icon}/>Pull Request</Typography>}  />
+            <Tab label={<Typography className = {classes.tablabel}><ActionIcon className={classes.icon}/>Actions</Typography>} />
+            <Tab label={<Typography className = {classes.tablabel}><ProjectIcon className={classes.icon}/>Projects</Typography>} />
+            <Tab label={<Typography className = {classes.tablabel}><SecurityIcon className={classes.icon}/>Security</Typography>}  />
+            <Tab label={<Typography className = {classes.tablabel}><InsightIcon className={classes.icon}/>Insights</Typography>} />
           </Tabs>
         </Grid>
 
-        <Grid container ref={this.myRef}>
+        <Grid container ref={this.myRef} className = {classes.issuesGrid}>
+          <Grid item xs = {12} className = {classes.secondHeader}>
+            <Typography>
+              <CircleIcon className = {classes.icon} style = {{color : "#000"}}/> 625 Open 
+              <CheckIcon className={classes.checkIcon}/>10,140 Closed
+            </Typography>
+          </Grid>
           <InfiniteScroll
             dataLength={this.state.data.length}
             next={this.fetch}
-            hasMore={this.state.hasMore}
-            loader={<h4>Loading...</h4>}>
+            style = {{width: this.props.width === "xs" || this.props.width === "xl" ? "100vw" : "100%", overflowX : ""}}
+            hasMore={this.state.hasMore}>
               {this.state.data && this.state.data.length > 0 && this.state.data.map((item, index) => (
                 <Grid item xs = {12} key = {index}>
                   <IssuesItem title = {item.title} 
                               issueNo = {item.number}
                               createdAt = {item.created_at}
                               userName = {item.user.login}
+                              comments = {item.comments}
                               labels = {item.labels}/>
                 </Grid>
               ))}
@@ -138,4 +221,4 @@ class App extends React.Component {
   }
 }
 
-export default withStyles(style)(App);
+export default withWidth()(withStyles(style)(App));
--- src/IssuesItem.js
+++ src/IssuesItem.js
@@ -2,15 +2,18 @@ import "./styles.css";
 import React from "react";
 
 import { withStyles } from '@material-ui/core/styles';
-import {Grid, Typography, Card, Chip} from '@material-ui/core'
+import {Grid, Typography, Card, Chip, Hidden} from '@material-ui/core'
 import CircleIcon from '@material-ui/icons/Adjust';
-
+import Comment from '@material-ui/icons/ChatBubbleOutline';
 import {timeSince} from "./ServiceClass"
 
 const style = theme => ({
   card : {
     padding : "20px",
-    // display : "flex"
+    cursor : "pointer",
+    '&:hover': {
+      background: "rgba(0, 0, 0, 0.07)",
+   },
   },
   title : {
     fontWeight : "600",
@@ -19,8 +22,29 @@ const style = theme => ({
   },
   chip : {
     margin : "0px 0px 0px 10px",
+    [theme.breakpoints.down('md')]: {
+      marginTop : "5px",
+      marginBottom : "5px"
+    },
     fontWeight : 600
   },
+  comments : {
+    display : "inline",
+    float : "right",
+    color : "grey",
+    fontSize : "13px"
+  },
+  commentIcon : {
+    fontSize : "15px",
+    margin: "0px 5px 0px 0px",
+    verticalAlign : "middle"
+  },
+  info : {
+    color : "grey",
+    fontSize : "13px",
+    margin : "0px 0px 0px 10px",
+    display : "inline"
+  }
 })
 
 const chipColor = {
@@ -38,6 +62,10 @@ class IssuesItem extends React.Component {
     // console.log(this.props.labels)
   }
 
+  onClick = () => {
+    console.log("click")
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -46,22 +74,51 @@ class IssuesItem extends React.Component {
     let time = noOfDays === "1 days" ? "yesterday" : noOfDays + " ago"
 
     return (
-      <Card className = {classes.card}> 
-        <CircleIcon style={{ color: "green", alignSelf : "center"}}/>
-        <Typography variant = "h6" className = {classes.title}>{this.props.title}</Typography>
-        {this.props.labels && this.props.labels.length > 0 && this.props.labels.map(item => (
-          <Chip label={item.name} 
-                variant="outlined" 
-                size = "small" 
-                style = {{backgroundColor : (chipColor[item.name] && chipColor[item.name].bgColor) || "#F5F5DC", 
-                          color : (chipColor[item.name] && chipColor[item.name].fColor) || "black"}}
-        className={classes.chip} />
-        ))}
-        <div>
-          <Typography>
-            #{this.props.issueNo} opened at {time} by {this.props.userName}
-          </Typography>
-        </div>
+      <Card className = {classes.card} onClick = {this.onClick}> 
+        <Grid container> 
+          <Grid item>
+            <CircleIcon style={{ color: "green", marginTop : "4px"}}/>
+          </Grid>
+          <Grid item xs>
+            <Typography variant = "subtitle1" className = {classes.title}>{this.props.title}</Typography>
+            {this.props.labels && this.props.labels.length > 0 && this.props.labels.map(item => (
+              <Chip label={item.name} 
+                    variant="outlined" 
+                    size = "small" 
+                    style = {{backgroundColor : (chipColor[item.name] && chipColor[item.name].bgColor) || "#F5F5DC", 
+                              color : (chipColor[item.name] && chipColor[item.name].fColor) || "black"}}
+            className={classes.chip} />
+            ))}
+            <Hidden mdDown>
+              <div style = {{marginTop : "10px"}}>
+                <Typography className = {classes.info}>
+                  #{this.props.issueNo} opened at {time} by {this.props.userName}
+                </Typography>
+                {this.props.comments 
+                  ? <Typography className={classes.comments}>
+                      <Comment className = {classes.commentIcon}/>{this.props.comments}
+                    </Typography> 
+                  : null}
+              </div>
+            </Hidden>
+            <Hidden mdUp>
+              <Grid container style = {{marginTop : "10px"}}>
+                <Grid item xs>
+                  <Typography className = {classes.info}>
+                    #{this.props.issueNo} opened at {time} by {this.props.userName}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                {this.props.comments 
+                  ? <Typography className={classes.comments}>
+                      <Comment className = {classes.commentIcon}/>{this.props.comments}
+                    </Typography> 
+                  : null}
+                </Grid>
+              </Grid>
+            </Hidden>
+            </Grid>
+        </Grid>
       </Card>
     );
   }
